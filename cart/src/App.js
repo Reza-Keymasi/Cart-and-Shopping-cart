37bbe3014a7a0228a4c38de151bc1c1b619bf9e4
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-06-14 22:12:06 +0430

    I use Route  and difine the path for Route(s) that when client tried to click on somethin like deatails, the deatails page get render and shown to client. Also if client put everything after products/ , he/she will be redirect on products path

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
 
 // Components
 import Store from "./components/Store";
+import ProductDetails from './components/shared/ProductDetails';
 
 // Context
 import ProductContextProvider from './context/ProductContextProvider'
 
+
 const App = () => {
   return (
     <ProductContextProvider>
-      <Store />
+      <Switch>
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/products" component={Store} />
+        <Redirect to="/products"/>
+      </Switch>
     </ProductContextProvider>
   );
 };
