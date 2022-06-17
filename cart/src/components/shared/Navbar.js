bf2ec0fb105ed28e2a9b151932c4be958a5481fd
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-06-17 14:23:54 +0430

    Navigation bar created and icons were added.

---
+++ cart/src/components/shared/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Link } from "react-router-dom"
+
+// context
+import { CartContext } from '../../context/CartContextProvider';
+
+// Icons
+import Cart from "../../assets/icons/Cart.svg"
+
+const Navbar = () => {
+
+    const { state } = useContext(CartContext)
+
+    return (
+        <div>
+            <Link to="/products">Products</Link>
+            <div>
+               <Link to="/cart"> <img src={Cart} style={{width: "50px", height: "50px", color: "blue"}} alt="shop"/></Link>
+                <span>{state.itemsCounter}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
