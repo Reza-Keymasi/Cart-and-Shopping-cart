8c98034d3cbac477c32ea633e5b02d525e38d74d
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-06-17 14:27:43 +0430

    defined sumItems that count number of items and it will use to show number of items on the icon in the Navbar

--- cart/src/context/CartContextProvider.js
+++ cart/src/context/CartContextProvider.js
@@ -7,6 +7,12 @@ const initialState = {
     checkout : false
 }
 
+const sumItems = items => {
+    const itemsCounter = items.reduce((total, product) => total + product.quantity, 0);
+    let total = items.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2);
+    return { itemsCounter, total }
+}
+
 const cartReducer = (state, action) => {
     switch (action.type) {
         case "ADD_ITEM":
@@ -18,13 +24,15 @@ const cartReducer = (state, action) => {
             }
             return {
                 ...state,
-                selectedItems: [...state.selectedItems]
+                selectedItems: [...state.selectedItems],
+                ...sumItems(state.selectedItems),
             }
         case "REMOVE_ITEM":
             const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload);
             return {
                 ...state,
-                selectedItems: [...newSelectedItems]
+                selectedItems: [...newSelectedItems],
+                ...sumItems(state.selectedItems),
             }
 
         case "INCREASE":
@@ -32,12 +40,14 @@ const cartReducer = (state, action) => {
             state.selectedItems[indexI].quantity++;
             return {
                 ...state,
+                ...sumItems(state.selectedItems),
             }
         case "DECREASE":
             const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id);
             state.selectedItems[indexD].quantity--;
             return {
                 ...state,
+                ...sumItems(state.selectedItems),
             }
         case "CHECKOUT":
             return {
